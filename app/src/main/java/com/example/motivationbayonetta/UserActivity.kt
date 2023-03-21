5d2f89f16a2dcb5ead77b9d92300e6de7b0be659
MikhaelVinicius <mickaelsoel@gmail.com>
2023-03-21 10:51:14 -0300

    UserActivity criado

---
+++ app/src/main/java/com/example/motivationbayonetta/UserActivity.kt
@@ -0,0 +1,30 @@
+package com.example.motivationbayonetta
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.motivationbayonetta.databinding.ActivityUserBinding
+
+class UserActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityUserBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonSave.setOnClickListener(this)
+
+
+        supportActionBar?.hide()
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.button_save) {
+
+        }
+
+    }
+}
