481085f36859c6ce6a2a783db00e69338f81f491
MikhaelVinicius <mickaelsoel@gmail.com>
2023-04-02 10:12:45 -0300

    Frases adicionadas, projeto concluido e em entrando em fase de testes

--- app/src/main/java/com/example/motivationbayonetta/repo/Mock.kt
+++ app/src/main/java/com/example/motivationbayonetta/repo/Mock.kt
@@ -13,7 +13,25 @@ class Mock {
     private val sunny = MotivationConstants.PHRASEFILTER.SUNNY
 
     private val listPhrases: List<Phrase> = listOf(
-    
+        Phrase("Vamos ao rock, baby!", happy),
+        Phrase("Você é tudo o que eu desejo\n" +
+                "Tudo o que eu cultuo e adoro\n" +
+                "Em outras palavras, por favor seja verdadeiro\n" +
+                "Em outras palavras, eu te amo", happy),
+        Phrase("Let's dance, boys!", happy),
+        Phrase("Se você precisa aprender a falar com uma dama, pergunte a sua mãe!", happy),
+        Phrase("Acha que você tem uma chance?", happy),
+        Phrase("Tente acompanhar!", happy),
+        Phrase("Isso é tudo que você tem?", happy),
+        Phrase("Se vou perder tempo, prefiro fazê-lo em um bom banho quente.", happy),
+
+        Phrase("Já é hora de me mexer de novo.", sunny),
+        Phrase("Perdeu seu terninho?", sunny),
+        Phrase("Não há necessidade de ser gentil.", sunny),
+        Phrase("Você tem sido travesso!", sunny),
+        Phrase("Meoww", sunny),
+        Phrase("Gostou do meu novo corte de cabelo?", sunny),
+
     )
 
     // Obtém frase aleatória de acordo com o filtro
