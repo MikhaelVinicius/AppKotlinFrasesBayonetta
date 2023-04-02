a5f7d8c7dd17f50f653f4d71f2ac5f339abfc2a3
MikhaelVinicius <mickaelsoel@gmail.com>
2023-04-02 09:24:19 -0300

    Salvamento de nome

--- app/src/main/java/com/example/motivationbayonetta/UserActivity.kt
+++ app/src/main/java/com/example/motivationbayonetta/UserActivity.kt
@@ -1,11 +1,15 @@
 package com.example.motivationbayonetta
 
+
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import com.example.motivationbayonetta.databinding.ActivityUserBinding
+import com.example.motivationbayonetta.SecurityPreferences
 
 class UserActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -32,8 +36,12 @@ class UserActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun handleSave() {
+
         val name = binding.editName.text.toString()
         if (name != "") {
+
+            SecurityPreferences(this).storeString("USER_NAME", name)
+
             startActivity(Intent(this, MainActivity::class.java))
             finish()
 
